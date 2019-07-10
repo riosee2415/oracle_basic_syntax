d92137be80c2fbf58989f4f9b9baa30c23e7484a
DEV_YUN <riosee2415@gmail.com>
2019-07-10 13:52:00 +0900

    update
    update

--- JOIN_문제2_SCOTT.sql
+++ JOIN_문제2_SCOTT.sql
@@ -34,4 +34,48 @@ SELECT
    
    
    
---문제4) 커미션을 받는 사원의 이름과 그가 속한 부서명을 출력하세요.
+--문제4) 커미션을 받는 사원의 이름과 그가 속한 부서명, 커미션을 출력하세요.
+--      단, 0원의 커미션을 받는 사원은 제외합니다.
+SELECT
+        A.ENAME,
+        B.DNAME,
+        A.COMM
+  FROM  EMP     A
+ INNER 
+  JOIN  DEPT    B
+    ON  A.DEPTNO    = B.DEPTNO
+   AND  A.COMM IS NOT NULL
+   AND  A.COMM <> 0;
+   
+   
+   
+--문제5) 뉴욕에 근무하는 사원의 사원번호, 이름, 급여, 근무지를 출력하세요.
+SELECT * FROM DEPT;
+
+SELECT
+        A.EMPNO,
+        A.ENAME,
+        A.SAL,
+        B.LOC
+  FROM  EMP     A
+ INNER
+  JOIN  DEPT    B
+    ON  A.DEPTNO    = B.DEPTNO
+   AND  B.LOC       = 'NEW YORK';
+   
+   
+   
+--문제6) 급여가 1200이하인 사원들의 사원번호, 이름, 급여, 근무지를 출력하세요.
+SELECT
+        A.EMPNO,
+        A.ENAME,
+        A.SAL,
+        B.LOC
+  FROM  EMP     A
+ INNER
+  JOIN  DEPT    B
+    ON  A.DEPTNO    = B.DEPTNO
+   AND  A.SAL       <= 1200;
+   
+   
+   
